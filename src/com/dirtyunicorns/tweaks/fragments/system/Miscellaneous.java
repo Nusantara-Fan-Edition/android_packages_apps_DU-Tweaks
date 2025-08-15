8f2f652971a0157c0611d78d487f4d4964243a45
neophyte721 <alprjkt@gmail.com>
2025-08-15 07:57:34 +0000

    Revert "Nad: Mn: User toggle for StreamProp [2/2]"
    This reverts commit 62cf431ff2655d5fb071ef0cfc2dead9c193f548.

--- src/com/dirtyunicorns/tweaks/fragments/system/Miscellaneous.java
+++ src/com/dirtyunicorns/tweaks/fragments/system/Miscellaneous.java
@@ -53,10 +53,8 @@ public class Miscellaneous extends SettingsPreferenceFragment
     private static final String KEY_RINGTONE_FOCUS_MODE_V2 = "ringtone_focus_mode_v2";
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
     private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    private static final String KEY_STREAM_SPOOF = "use_stream_spoof";
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
-    private static final String SYS_STREAM_SPOOF = "persist.sys.pixelprops.streaming";
 
     private SystemSettingMasterSwitchPreference mGamingMode;
     private ListPreference mScrollingCachePref;
@@ -64,7 +62,6 @@ public class Miscellaneous extends SettingsPreferenceFragment
     private ListPreference mRingtoneFocusMode;
     private SwitchPreference mGamesSpoof;
     private SwitchPreference mPhotosSpoof;
-    private SwitchPreference mStreamSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -104,10 +101,6 @@ public class Miscellaneous extends SettingsPreferenceFragment
         mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
         mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
         mPhotosSpoof.setOnPreferenceChangeListener(this);
-
-        mStreamSpoof = (SwitchPreference) findPreference(KEY_STREAM_SPOOF);
-        mStreamSpoof.setChecked(SystemProperties.getBoolean(SYS_STREAM_SPOOF, true));
-        mStreamSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -132,10 +125,6 @@ public class Miscellaneous extends SettingsPreferenceFragment
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
-        } else if (preference == mStreamSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_STREAM_SPOOF, value ? "true" : "false");
-            return true;
         }
         return false;
     }
@@ -143,7 +132,6 @@ public class Miscellaneous extends SettingsPreferenceFragment
     public static void reset(Context mContext) {
         SystemProperties.set(SYS_GAMES_SPOOF, "false");
         SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
-        SystemProperties.set(SYS_STREAM_SPOOF, "true");
     }
 
     private static boolean hasPhysicalDisplayCutout(Context context) {
