d01ac977ea7ab2d4268b5d9975cba7dd92c6e639
neophyte <neophyteplayground@gmail.com>
2026-02-03 08:06:31 +0000

    Revert "DuTweaks: Restore Spoofing"
    This reverts commit 1a06699187b0153141e5d979dd725d0d4338a128.

--- src/com/dirtyunicorns/tweaks/fragments/system/Miscellaneous.java
+++ src/com/dirtyunicorns/tweaks/fragments/system/Miscellaneous.java
@@ -51,17 +51,11 @@ public class Miscellaneous extends SettingsPreferenceFragment
     private static final String SCROLLINGCACHE_DEFAULT = "2";
     private static final String PREF_KEY_CUTOUT = "cutout_settings";
     private static final String KEY_RINGTONE_FOCUS_MODE_V2 = "ringtone_focus_mode_v2";
-    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
-    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
-    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
-    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
 
     private SystemSettingMasterSwitchPreference mGamingMode;
     private ListPreference mScrollingCachePref;
     private ListPreference mScreenOffAnimation;
     private ListPreference mRingtoneFocusMode;
-    private SwitchPreference mGamesSpoof;
-    private SwitchPreference mPhotosSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -93,13 +87,6 @@ public class Miscellaneous extends SettingsPreferenceFragment
         if (!res.getBoolean(com.android.internal.R.bool.config_deviceRingtoneFocusMode)) {
             prefScreen.removePreference(mRingtoneFocusMode);
         }
-	mGamesSpoof = (SwitchPreference) prefScreen.findPreference(KEY_GAMES_SPOOF);
-        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, true));
-        mGamesSpoof.setOnPreferenceChangeListener(this);
-
-        mPhotosSpoof = (SwitchPreference) prefScreen.findPreference(KEY_PHOTOS_SPOOF);
-        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
-        mPhotosSpoof.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -115,22 +102,9 @@ public class Miscellaneous extends SettingsPreferenceFragment
             int index = mScreenOffAnimation.findIndexOfValue((String) newValue);
             mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[index]);
             Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_ANIMATION, value);
-	    return true;
-	} else if (preference == mGamesSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
-            return true;
-        } else if (preference == mPhotosSpoof) {
-            boolean value = (Boolean) newValue;
-            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
         return false;
-    }
-
-   public static void reset(Context mContext) {
-        SystemProperties.set(SYS_GAMES_SPOOF, "true");
-        SystemProperties.set(SYS_PHOTOS_SPOOF, "true");
     }
 
     private static boolean hasPhysicalDisplayCutout(Context context) {
